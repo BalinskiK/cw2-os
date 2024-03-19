2661bc9cc9696def1f39288d477b865576cce819
Kamil Balinski <102728966+BalinskiK@users.noreply.github.com>
2024-03-19 20:04:37 +0000

    upload memstats

--- fs/proc/memstats.c
+++ fs/proc/memstats.c
@@ -5,35 +5,90 @@
 #include <linux/mm.h>
 
 int proc_pid_memstats(struct seq_file *m, struct pid_namespace *ns, struct pid *pid, struct task_struct *task) {
-    int total_vm_areas = 0;
+    struct vm_area_struct *vma;
+    struct mm_struct *mm
+
+    int total_vmas = 0;
+    int biggest_vma_size = 0;
+    int readable_vmas = 0;
+    int writable_vmas = 0;
+    int executable_vmas = 0;
+    int shared_vmas = 0;
+    int private_vmas = 0;
+    int locked_vmas = 0;
+    int executable_image_vmas = 0;
+    int file_backed_vmas = 0;
+    int anonymous_vmas = 0;
+
     int total_physical_pages = 0;
     int swapped_out_pages = 0;
+    int read_only_pages = 0;
+    int writable_pages = 0;
+    int shared_pages = 0;
+    int special_pages = 0;
+    int huge_pages = 0;
     // Other memory statistics...
 
     // Lock the task's memory descriptor to ensure data integrity
     task_lock(task);
 
     // Gather memory statistics
-    struct mm_struct *mm = get_task_mm(task);
+    mm = get_task_mm(task);
     if (mm) {
-        struct vm_area_struct *vma = mm->mmap;
-        while (vma) {
-            total_vm_areas++;
-            vma = vma->vm_next;
-        }
-
-        total_physical_pages = get_mm_rss(mm);
-        // You can gather other memory statistics here...
+        vma = mm->mmap;
+        VMA_ITERATOR(vmi, mm, 0);
+        mmap_read_lock(mm);
+        for_each_vma(vmi, vma) {
+            // Count different types of VMAs
+            total_vm_count++;
+            if (vma->vm_end - vma->vm_start > biggest_vma_size)
+                biggest_vma_size = vma->vm_end - vma->vm_start;
+            if (vma->vm_flags & VM_READ)
+                readable_vm_count++;
+            if (vma->vm_flags & VM_WRITE)
+                writable_vm_count++;
+            if (vma->vm_flags & VM_EXEC)
+                executable_vm_count++;
+            if (vma->vm_flags & VM_SHARED)
+                shared_vm_count++;
+            else
+                private_vm_count++;
+            if (vma->vm_flags & VM_LOCKED)
+                locked_vm_count++;
+            if (vma->vm_flags & VM_EXECUTABLE)
+                executable_image_vm_count++;
+            if (vma->vm_file)
+                file_backed_vm_count++;
+            else
+                anonymous_vm_count++;
+        };
+        mmap_read_unlock(mm);
     }
 
     // Unlock the memory descriptor
     task_unlock(task);
 
-    // Write memory statistics to the seq_file
     seq_printf(m, "Virtual Memory Area Stats:\n");
-    seq_printf(m, "Total VMAs: %d\n", total_vm_areas);
-    seq_printf(m, "Total Physical Pages: %d\n", total_physical_pages);
-    // Write other memory statistics...
+    seq_printf(m, "Total VMAs: %d\n", total_vm_count);
+    seq_printf(m, "Biggest VMA Size: %lu\n", biggest_vma_size);
+    seq_printf(m, "Readable VMAs: %d\n", readable_vm_count);
+    seq_printf(m, "Writable VMAs: %d\n", writable_vm_count);
+    seq_printf(m, "Executable VMAs: %d\n", executable_vm_count);
+    seq_printf(m, "Shared VMAs: %d\n", shared_vm_count);
+    seq_printf(m, "Private VMAs: %d\n", private_vm_count);
+    seq_printf(m, "Locked VMAs: %d\n", locked_vm_count);
+    seq_printf(m, "Executable Image VMAs: %d\n", executable_image_vm_count);
+    seq_printf(m, "File Backed VMAs: %d\n", file_backed_vm_count);
+    seq_printf(m, "Anonymous VMAs: %d\n", anonymous_vm_count);
+
+    seq_printf(m, "Physical Pages Stats:\n");
+    seq_printf(m, "Total Physical Pages: %d\n", total_phys_pages);
+    seq_printf(m, "Number of Pages Swapped Out: %d\n", swapped_out_pages);
+    seq_printf(m, "Read-Only Pages: %d\n", read_only_pages);
+    seq_printf(m, "Writable Pages: %d\n", writable_pages);
+    seq_printf(m, "Number of Shared Pages: %d\n", shared_pages);
+    seq_printf(m, "Number of Special Pages: %d\n", special_pages);
+    seq_printf(m, "Number of Huge Pages: %d\n", huge_pages);
 
     return 0;
 }
